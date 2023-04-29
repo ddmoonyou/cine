55efcf5077cc1a0dc22519327a6804d617918bd6
1Pz <pee.nrm@gmail.com>
2023-04-29 22:06:59 +0700

    Revert "Revert "Revert "Update"""
    This reverts commit 591ccfe453c613cd64ccfd8b3925cc8fca5443f4.

--- home/snack_temp.php
+++ home/snack_temp.php
@@ -2,10 +2,6 @@
 <html lang="en-US">
 
 <head>
-
-    <?php ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL); ?>
     <meta charset="UTF-8">
     <meta name="description" content="Booking Ticket Website">
     <meta name="keywords" content="Movie,Cine,Cinema">
@@ -16,8 +12,10 @@
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&family=Rubik+Glitch&family=Vollkorn&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,300&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&family=Rubik+Glitch&family=Vollkorn&display=swap"
+        rel="stylesheet">
+
     <!-- Css Styles -->
     <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
@@ -34,11 +32,11 @@
 
     <!-- Connect to database -->
     <?php
-    $servername = "127.0.0.1";  // server name localhost
-    $username = "root";    // username to access database id20613770_cinebyhansa
-    $password = "";    // password to access database %1tcYSTb0gf}1+YM
+    $servername = "127.0.0.1"; // server name localhost
+    $username = "root"; // username to access database id20613770_cinebyhansa
+    $password = ""; // password to access database %1tcYSTb0gf}1+YM
     $dbname = "cine"; // name of the database
-
+    
     // Create a connection to MySQL database
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -48,19 +46,19 @@
     }
 
     //echo "Connected successfully";
-
-    ?> 
+    
+    ?>
 
 </head>
 
 <body>
     <!-- Page Preloder -->
-    <div id="preloder">
+    <!-- <div id="preloder">
         <div class="loader"></div>
-    </div>
+    </div> -->
 
 
-    
+
 
     <!-- Header Section Begin -->
     <header class="header">
@@ -75,10 +73,10 @@
                     <nav class="header__menu mobile-menu">
                         <ul>
                             <li><a href="./index.php">Home</a></li>
-                            <li><a href="./theatre.php">Theatre</a></li>
-                            <li class="active"><a href="./movie.php">Movie</a></li>
-                            <li ><a href="./promotion.php">Promotion</a></li>
-                            <li><a href="./snack.php">Snack&Drink</a></li>
+                            <li><a href="./theatre.php">Branch</a></li>
+                            <!-- <li><a href="./movie.php">Movie</a></li> -->
+                            <li><a href="./promotion.php">Promotion</a></li>
+                            <li class="active"><a href="./snack.php">Snack&Drink</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -93,45 +91,13 @@
         </div>
     </header>
     <!-- Header Section End -->
-    
-
     <!-- Breadcrumb Section Begin -->
     <section class="breadcrumb-option">
-
-            <?php
-                  $movie_id = $_GET['movie_id'];
-                  $sql = "SELECT * FROM movieinfo WHERE movie_id = '$movie_id'";
-                  $result = mysqli_query($conn, $sql);
-                  $row = mysqli_fetch_assoc($result);
-                  $movie_name = $row['movie_name'];
-                  $description = $row['movie_description'];
-                  $movie_length = $row['movie_length'];
-                  $director = $row['director_info'];
-                  $_start_date = $row['releaseDate'];
-                  $_start_date = date('d M Y', strtotime($_start_date));
-                  $trailer = $row['movie_trailer'];
-            ?>
-
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb__text">
-                        <div class="row">
-                              <div class="image-container">
-                                    <img src ="./img/poster/<?php echo $movie_id?>.jpg" alt="Movie Poster" width="240" height="320" class="movie-poster">
-                                    <!-- <iframe width="560" height="315" src="<?php echo $trailer; ?>" 
-                                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                                    allowfullscreen></iframe> -->
-                              </div>
-                              <div class="col-lg-8 md-6 xd-6 d-flex align-items-center">
-                                    <ul><h2 class="h2-movie"><?php echo $movie_name ?></h2><ul>
-                                    <ul><p> <?php echo $description ?><p><ul>
-                                    <ul><p>Release Date: <?php echo $_start_date ?><p><ul>
-                                    <ul><p>Director: <?php echo $director ?><p><ul>
-                                    <ul><p>Length: <?php echo $movie_length ?>min<p><ul>
-
-                              </div>
-                        </div> 
+                        <h4>Snack & Drink</h4>
                     </div>
                 </div>
             </div>
@@ -139,71 +105,122 @@
     </section>
     <!-- Breadcrumb Section End -->
 
-    <!-- Query a showings of each movie -->
-    <?php
-    $sql = "SELECT s.*, b.branch_name, t.*
-    FROM (
-        SELECT DATE(date_time) AS date_only
-        FROM showings
-        WHERE movie_id = $movie_id
-        AND date_time BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 3 DAY)
-        GROUP BY DATE(date_time)
-        ) d
-        INNER JOIN showings s ON DATE(s.date_time) = d.date_only
-        INNER JOIN branchinfo b ON s.branch_id = b.branch_id 
-        INNER JOIN theaterinfo t ON s.theater_no = t.theater_no
-        WHERE s.movie_id = $movie_id
-        AND s.date_time BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 3 DAY)
-        ORDER BY s.date_time ASC";
-    $result = mysqli_query($conn, $sql);
-    $displayed_date = null;
-    ?>
-
-
-    <!-- Product Section Begin -->
-    <section class="blog spad">
-            <?php       // generate each showing
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $showingID = $row['showing_id'];
-                    $branch_name = $row['branch_name'];
-                    $date_time = $row['date_time'];
-                    $time = date('h:i', strtotime($date_time));
-                    $date = date('d M Y', strtotime($date_time));
-                    $theatre_no = $row['theater_no'];
-                    $language_sub = $row['language_sub'];
-                    $language_dub = $row['language_dub'];
-                    $system_type = $row['system_type'];
-                    
-                    if ($date !== $displayed_date) {
-                        // display the date and mark it as displayed
-                        $displayed_date = $date;
-                        echo '<div class="blog__alt"><div class="blog__item__alt"><h3>' . $date . '</h3></div></div>';
-                    }
-            ?>
-            
-            <div class="container">
-                <div class="row no-gutters">
-                    <div class="col-lg-12 md-8 xs-3">
-                        <div class="showings-container" data-movie-id="<?php echo $showingID; ?>">
-                            <a href="reservation.php?showing_id=<?php echo $showingID; ?>">
-                                <div class="blog__item">                  
-                                    <div class="blog__item__text">
-                                        <h5><img src="./img/icon/clock.png" width=20px height=20px> <?php echo $time; ?></h5>
-                                        <h6><img src="./img/icon/location.png" width=20px height=20px> <?php echo $branch_name; ?></h6>
-                                        <p>Theatre No: <?php echo $theatre_no; ?> </p>
-                                        <p><img src="./img/icon/video-camera.png" width=20px height=20px> System Type: <?php echo $system_type ?> </p>   
+    <!-- Shop Section Begin -->
+    <section class="shop spad">
+       <div class="container">
+           <div class="row">
+                <div class="col-lg-3">
+                    <div class="shop__sidebar">
+                        <div class="shop__sidebar__search">
+                            <form action="#">
+                                <input type="text" placeholder="Search...">
+                                <button type="submit"><span class="icon_search"></span></button>
+                            </form>
+                        </div>
+                        <div class="shop__sidebar__accordion">
+                            <div class="accordion" id="accordionExample">
+                                <div class="card">
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapseSix">Tags</a>
+                                    </div>
+                                    <div id="collapseSix" class="collapse show" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <div class="shop__sidebar__tags">
+                                                <a href="#">Drinks</a>
+                                                <a href="#">Snacks</a>
+                                                <a href="#">Popcorn</a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="shop__product__option">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-6">
+                                <div class="shop__product__option__left">
+                                    <p>Showing 1–12 of 126 results</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6">
+                                <div class="shop__product__option__right">
+                                    <p>Sort by Price:</p>
+                                    <select>
+                                        <option value="">Low To High</option>
+                                        <option value="">$0 - $55</option>
+                                        <option value="">$55 - $100</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- Qurey a data -->
+                    <?php
+                        $sql = "SELECT * FROM foodinfo  ORDER BY food_type";
+                        $result = $conn->query($sql);
+                        $current_food_type = null;
+                    ?>
+
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 col-sm-6">
+                            <?php 
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $food_type = $row['food_type'];
+                                    $category = $row['category'];
+                                    $description = $row['description'];
+                            ?>
+                            
+                            <div class="product__item">
+                                
+                                <div class="product__item__text">
+                                
+                                    <?php if ($current_food_type != $food_type) { ?>
+                                        <img src="./img/snack/<?php echo $food_type ?>.png" width="100" height="200">
+                                        <h6><?php echo $food_type; ?></h6>
+                                        
+                                    <?php  }
+                                        $current_food_type = $food_type;
+                                        $sql = "SELECT * FROM foodsize WHERE food_type = '$current_food_type'";
+                                        $result = $conn->query($sql);
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                          $size = $row['size'];
+                                          $price= $row['price'];
+                                          $food_id = $row['food_id'];
+                                        
+                                    ?>
+                                          <h6><?php echo $size.$price; } ?></h6>
+
+                                    
+                                </div>
+                                
+                            </div>
+                            <?php } ?>    
+                        </div>
+                    </div>
+                    
+                    
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="product__pagination">
+                                <a class="active" href="#">1</a>
+                                <a href="#">2</a>
+                                <a href="#">3</a>
+                                <span>...</span>
+                                <a href="#">21</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-       
-        <?php } ?>
     </section>
-    <!-- Product Section End -->
+    <!-- Shop Sectio End -->
+
 
     <!-- Footer Section Begin -->
     <footer class="footer">
@@ -212,10 +229,10 @@
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="footer__about">
                         <div class="footer__logo">
-                            <a href="./index.php"><img src="img/about/icon-alt.png" alt=""></a>
+                            <a href="#"><img src="img/about/icon-alt.png" alt=""></a>
                         </div>
-                        <p>Best Cinema in SEA.</p>
-                    
+                        <p>Best Cinema in SEA</p>
+
                     </div>
                 </div>
                 <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
@@ -294,12 +311,3 @@
 </body>
 
 </html>
-
-
-
-
-<!-- SELECT s.*, b.branch_name, t.* FROM showings s 
-            INNER JOIN branchinfo b ON s.branch_id = b.branch_id 
-            INNER JOIN theaterinfo t ON s.theater_no = t.theater_no            
-            WHERE s.date_time >= CURDATE() AND s.date_time < DATE_ADD(CURDATE(), INTERVAL 1 DAY) 
-            AND s.movie_id = $movie_id -->
