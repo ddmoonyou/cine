c6f3fed3a984388517abdbd1e4e552f0573a6a3c
ddmoonyou <nuchpawee.peam@kmutt.ac.th>
2023-05-23 21:40:36 +0700

    Edit promote

--- home/index.php
+++ home/index.php
@@ -98,7 +98,7 @@
     <section class="hero">
         <div class="hero__slider owl-carousel">
         <?php
-            $sql = "SELECT * FROM movieinfo  WHERE promote = 1";
+            $sql = "SELECT * FROM movieinfo  WHERE promote >= CURRENT_DATE() or releaseDate <= CURRENT_DATE() and promote != NULL";
             $result = mysqli_query($conn, $sql);
             if (!$result) {
                 die('Invalid query: ' . mysqli_error($conn));
