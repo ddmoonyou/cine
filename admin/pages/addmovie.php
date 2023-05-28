ad03d5e557d38a8a29d4b4c0e041590778baccfa
om2546 <chayarob.chan@gmail.com>
2023-05-28 21:12:06 +0700

    Update addmovie.php

--- admin/pages/addmovie.php
+++ admin/pages/addmovie.php
@@ -45,10 +45,17 @@
 
         if($end_date < $start_date)
         {
-            echo "<script> alert('End date must be after start date!'); window.location.href='new-movie.php'; </script>";
+            $_SESSION['status'] = 'End date must be after start date!';
+            $_SESSION['status_text'] = 'Please try again';
+            $_SESSION['status_code'] = 'error';
+            header('Location: new-movie.php');
+
         } else if($premiere_time > $r_date)
         {
-            echo "<script> alert('Premiere date must be before release date!'); window.location.href='new-movie.php'; </script>";
+            $_SESSION['status'] = 'Premiere date must be before release date!';
+            $_SESSION['status_text'] = 'Please try again';
+            $_SESSION['status_code'] = 'error';
+            header('Location: new-movie.php');
         } else{
 
             $name =  mysqli_real_escape_string($con,$_POST["name"]);
@@ -64,42 +71,136 @@
             $time = $_POST["time"];
             $audio =  mysqli_real_escape_string($con,$_POST["audio"]);
             $subtitle =  mysqli_real_escape_string($con,$_POST["subtitle"]);
-
             $start_promote = date ('Y-m-d H:i:s', strtotime($sp_date));
             $end_promote = date ('Y-m-d H:i:s', strtotime($np_date));
             $releaseDate = date ('Y-m-d H:i:s', strtotime($r_date));
-            $sql ="INSERT INTO movieinfo(movie_name,movie_length,movie_description,director_info,start_promote,end_promote,releaseDate,movie_trailer)
-                    VALUES ('$name',$length,'$description','$director','$start_promote','$end_promote','$releaseDate','$movie_trailer');";
 
-            if (!mysqli_query($con, $sql)) {
-		    	die('Error: ' . mysqli_error($con));
-		    }
-            $id = mysqli_insert_id($conn);
+            $sql = "SELECT theater_no FROM theaterinfo
+                WHERE branch_id = $branch_id;";
+       
 
-            $filename = $_FILES["inputPosterImage"]["name"];
-            $path = "../../home/img/poster/".$id.".jpg";
-            move_uploaded_file($_FILES['inputPosterImage']['tmp_name'],$path);
+            $result = mysqli_query($con, $sql);
+            if (!$result) {
+                die('Error: ' . mysqli_error($con));
+            }
+            $teather = array();
+            foreach($result as $t_no)
+            {
+                array_push($teather,$t_no['theater_no']);
+            }
 
-            $filename = $_FILES["inputPromoteImage"]["name"];
-            $path = "../../home/img/promote/".$id.".jpg";
-            move_uploaded_file($_FILES['inputPromoteImage']['tmp_name'],$path); 
+            
+            if(in_array($theater,$teather))
+            {
+                $sql ="SELECT s.showing_id,s.date_time,s.movie_id,m.movie_length FROM showings s
+                    LEFT JOIN movieinfo m ON s.movie_id = m.movie_id
+                    WHERE s.branch_id = $branch_id AND s.theater_no = $theater;";
+                $result = mysqli_query($con, $sql);
+                if (!$result) {
+                    die('Error: ' . mysqli_error($con));
+                }
+                $all_time = array();
+                foreach($result as $ti)
+                {
+                    $temp = array();
+                    $temp['date_time'] = $ti['date_time'];
+                    $temp['len'] = $ti['movie_length'];
+                    $temp['new'] = 0;
+                    array_push($all_time,$temp);
+                }
+
+                
+
+                $len = mysqli_fetch_assoc($result)['movie_length'];
+
+                $mysqltime = date ('Y-m-d H:i:s', strtotime($time));
+                $temp = array();
+                $temp['date_time'] = $mysqltime;
+                $temp['len'] = $len;
+                $temp['new'] = 1;
+                array_push($all_time,$temp);
+                
+
+                function compareDates($date1, $date2){
+                    return strtotime($date1['date_time']) - strtotime($date2['date_time']);
+                }
+
+                usort($all_time, "compareDates");
+            
+                
+                $status = 0;
+                $count = count($all_time);
+                for($i=0;$i<$count-1;$i++)
+                {
+                    
+                    $date1 = strtotime($all_time[$i]['date_time']);
+                    
+                    $date2 = strtotime($all_time[$i+1]['date_time']);
+                    $interval = $date2 - $date1;
+                    $minutes = floor($interval  / 60);
+                    if($i>0 && ($all_time[$i]['new']==1 ||$all_time[$i+1]['new']==1 ))
+                    {
+                        if($minutes<$all_time[$i]['len'])
+                        {
+                            echo $i."<br>".$all_time[$i]['date_time']."  ".$all_time[$i+1]['date_time']." ".$all_time[$i]['len'] ."<br>";
+                            $status = 1;
+                        }
+                    }
+                    
+                
+                    
+                }
+
+                if($status==1)
+                {
+                    $_SESSION['status'] = 'Showing time overlaps!';
+                    $_SESSION['status_text'] = 'Please try again';
+                    $_SESSION['status_code'] = 'error';
+                    header('Location: new-movie.php');
+                }
+                else
+                {
+                    $sql ="INSERT INTO movieinfo(movie_name,movie_length,movie_description,director_info,start_promote,end_promote,releaseDate,movie_trailer)
+                            VALUES ('$name',$length,'$description','$director','$start_promote','$end_promote','$releaseDate','$movie_trailer');";
+
+                    if (!mysqli_query($con, $sql)) {
+                        die('Error: ' . mysqli_error($con));
+                    }
+                    $id = mysqli_insert_id($conn);
+
+                    $filename = $_FILES["inputPosterImage"]["name"];
+                    $path = "../../home/img/poster/".$id.".jpg";
+                    move_uploaded_file($_FILES['inputPosterImage']['tmp_name'],$path);
+
+                    $filename = $_FILES["inputPromoteImage"]["name"];
+                    $path = "../../home/img/promote/".$id.".jpg";
+                    move_uploaded_file($_FILES['inputPromoteImage']['tmp_name'],$path); 
 
 
-            $mysqltime = date ('Y-m-d H:i:s', strtotime($time));
-            $sql ="INSERT INTO showings(movie_id,branch_id,theater_no,date_time,language_dub,language_sub,premiere_ticket)
-                    VALUES ($id,$branch_id,$theater,'$mysqltime','$audio','$subtitle',1);";
+                    $mysqltime = date ('Y-m-d H:i:s', strtotime($time));
+                    $sql ="INSERT INTO showings(movie_id,branch_id,theater_no,date_time,language_dub,language_sub,premiere_ticket)
+                            VALUES ($id,$branch_id,$theater,'$mysqltime','$audio','$subtitle',1);";
 
-            if (!mysqli_query($con, $sql)) {
-		    	die('Error: ' . mysqli_error($con));
-		    }
+                    if (!mysqli_query($con, $sql)) {
+                        die('Error: ' . mysqli_error($con));
+                    }
 
-            $_SESSION['status'] = 'Successful!';
-            $_SESSION['status_text'] = 'Added new movie';
-            $_SESSION['status_code'] = 'success';
-            header('Location: new-movie.php');
-            // echo "<script> alert('Add new movie successful!'); window.location.href='new-movie.php'; </script>";
+                    $_SESSION['status'] = 'Successful!';
+                    $_SESSION['status_text'] = 'Added new movie';
+                    $_SESSION['status_code'] = 'success';
+                    header('Location: new-movie.php');
+                    // echo "<script> alert('Add new movie successful!'); window.location.href='new-movie.php'; </script>";
+                }
+            } 
+            else
+            {
+                $_SESSION['status'] = 'Theater number not found!';
+                $_SESSION['status_text'] = 'Please try again';
+                $_SESSION['status_code'] = 'error';
+                header('Location: new-movie.php');
+            }
+
         }
-
     }
 
 ?>
