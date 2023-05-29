158af5e1bcc46ef38a9c1b7cc377187e7b208d72
1Pz <pee.nrm@gmail.com>
2023-05-29 17:11:33 +0700

    update selectbranchdate

--- home/selectbranch.php
+++ home/selectbranch.php
@@ -10,11 +10,13 @@
     
     $sql = "SELECT DISTINCT * FROM showings s,theaterinfo t,branchinfo b
                 where s.theater_no = t.theater_no
+                and b.branch_id = $branchID
+                and t.branch_id = $branchID
+                and s.branch_id = $branchID
                 and s.branch_id = t.branch_id
                 and b.branch_id = s.branch_id
                 and s.movie_id = $movieID
-                and date(s.date_time) = date('$dateID')
-                and b.branch_id = $branchID
+                and date(s.date_time) = date('$dateID') /* CURRENT_DATE() */
                 ORDER BY s.date_time ASC;
                 ";
     $res = mysqli_query($conn, $sql);
@@ -24,16 +26,17 @@
     // $date_time = $row['date_time'];
     // $date = date('d M Y', strtotime($date_time));
 
-    /*
-    echo ($row['date_time']);
+    
+    // echo ($row['']);
     ?><br><?php
     // echo ($date);
     ?><br><?php
     echo ($dateID);
-    */
+    
 
-    if($row != "" )
+    if($row != "")
     {
+        echo $sql;
 
         ?>
             <div class="container">
@@ -45,32 +48,59 @@
                                     <div class="card-top-showing">
                                         <h6 style="color:white"><img src="./img/icon/location.png" width=20px height=24px> <?php  echo $row['branch_name']; ?></h6>
                                     </div>
-                                        
+                                    
+                                    <?php
+                                    /* print ข้างนอกก่อน 1 รอบ */
+                                        $showingID = $row['showing_id'];
+                                        $branch_name = $row['branch_name'];
+                                        $date_time = $row['date_time'];
+                                        $time = date('H:i', strtotime($date_time));
+                                        $date = date('d M Y', strtotime($date_time));
+                                        $theatre_no = $row['theater_no'];
+                                        $language_sub = $row['language_sub'];
+                                        $language_dub = $row['language_dub'];
+                                        $system_type = $row['system_type'];
 
-                        <?php
-
-                        while ($row = mysqli_fetch_assoc($res)) {
-                            $showingID = $row['showing_id'];
-                            $branch_name = $row['branch_name'];
-                            $date_time = $row['date_time'];
-                            $time = date('H:i', strtotime($date_time));
-                            $date = date('d M Y', strtotime($date_time));
-                            $theatre_no = $row['theater_no'];
-                            $language_sub = $row['language_sub'];
-                            $language_dub = $row['language_dub'];
-                            $system_type = $row['system_type'];
-
-                            ?>          
+                                    ?>          
                                         <div class="card-showing-alt">
-                                        <p><img src="./img/icon/video-camera.png" width=20px height=20px> System Type: <?php echo  $row['system_type']; ?> </p>
-                                        <p><img src="./img/icon/audio.png" width=25px> <?php echo $row['language_dub'].'/'.$row['language_sub']; ?></p>
-                                        <img src="./img/icon/clock.png" width=25px height=25px>
-                                        <a href="reservation.php?showing_id=<?php echo $row['showing_id']; ?>">                                      
-                                        <button class="btn showing-time-btn" data-showingid="<?php echo $showingID?>"> 
-                                            <?php echo $time; ?>
-                                        </button> </div>
-                        <?php }  ?>
-                                    </a>
+                                            <p><img src="./img/icon/video-camera.png" width=20px height=20px> System Type: <?php echo  $row['system_type']; ?> </p>
+                                            <p><img src="./img/icon/audio.png" width=25px> <?php echo $row['language_dub'].'/'.$row['language_sub']; ?></p>
+                                            <img src="./img/icon/clock.png" width=25px height=25px>
+                                            <a href="reservation.php?showing_id=<?php echo $row['showing_id']; ?>">                                      
+                                            <button class="btn showing-time-btn" data-showingid="<?php echo $showingID?>"> 
+                                                <?php echo $time; ?>
+                                            </button> 
+                                            </a>
+                                        </div>
+                                    <?php
+
+                                    /* while loop print จนครบทุกรอบ */
+                                    while ($row = mysqli_fetch_assoc($res)) {
+                                        $showingID = $row['showing_id'];
+                                        $branch_name = $row['branch_name'];
+                                        $date_time = $row['date_time'];
+                                        $time = date('H:i', strtotime($date_time));
+                                        $date = date('d M Y', strtotime($date_time));
+                                        $theatre_no = $row['theater_no'];
+                                        $language_sub = $row['language_sub'];
+                                        $language_dub = $row['language_dub'];
+                                        $system_type = $row['system_type'];
+
+                                        ?>          
+                                                    <div class="card-showing-alt">
+                                                        <p><img src="./img/icon/video-camera.png" width=20px height=20px> System Type: <?php echo  $row['system_type']; ?> </p>
+                                                        <p><img src="./img/icon/audio.png" width=25px> <?php echo $row['language_dub'].'/'.$row['language_sub']; ?></p>
+                                                        <img src="./img/icon/clock.png" width=25px height=25px>
+                                                        <a href="reservation.php?showing_id=<?php echo $row['showing_id']; ?>">                                      
+                                                        <button class="btn showing-time-btn" data-showingid="<?php echo $showingID?>"> 
+                                                            <?php echo $time; ?>
+                                                        </button> 
+                                                        </a>
+                                                    </div>
+                                        <?php 
+                                    } 
+
+                                    ?>
                                 </div>
                             </div>
                         </div>
